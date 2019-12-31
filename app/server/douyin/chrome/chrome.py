23be22769229507be4e28c41a14e07436e9f5796
xiaohan008007 <xiaohan008007@163.com>
2019-12-31 17:33:09 +0800

    headless

--- app/server/douyin/chrome/chrome.py
+++ app/server/douyin/chrome/chrome.py
@@ -21,7 +21,7 @@ async def connect_chrome():
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36")
     await page.evaluate(webdriver_js)
     await page.goto("http://127.0.0.1:5000")
-    await asyncio.sleep(20000000)
+    await asyncio.sleep(2000)
     await page.close()
     await browser.close()
 
